fcbeb918bc2e7e7db3c4f86f50154cbcdb3f85ea
justaregularnormalguy <efunamoneme@gmail.com>
2024-12-14 00:41:42 +0000

    Submission

--- workspace/Ball.java
+++ workspace/Ball.java
@@ -1,11 +1,82 @@
+//  Class author:  Obiefuna Moneme
+//  Date created:  12/10/24
+//  General description: intializes the variables for ball and sets its parameters
 import java.awt.*;
 
 public class Ball {
 	//your code here!
-	
+	private int size;
+	private int x;
+	private int y;
+	private int xVelocity;
+	private int yVelocity;
 	//don't forget you need instance variables:
 	
+	public Ball (int x, int y, int size){
+	this.size = size;
+	this.x = x;
+	this.y = y;
+	xVelocity = 1;
+	yVelocity = 1;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets integer x to x
+	public void setX(int x) {
+		this.x = x;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets integer y to y
+	public void setY(int y) {
+		this.y = y;
+	}
+	// Pre-condition: none
+	//  Post-condition: serts the color of the ball to white
+	public void draw(Graphics g){
+		g.setColor(Color.white);
+		}
+	// Pre-condition: none
+	//  Post-condition: sets the x position
+	public int getXpos (){
+		return x;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets the y position
+	public int getYpos(){
+		return y;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets the direction int the x velocity
+	public void setXVelocity(int xVelocity){
+		this.xVelocity = xVelocity;
+	}
+	// Pre-condition: none
+	//  Post-condition:sets the velocity in the y direction
+	public void setYVelocity(int yVelocity){
+		this.yVelocity = yVelocity;
+	}
 	
+	// Pre-condition: x and y variables and their respective velocity variables are properly initialized
+	//  Post-condition: sets the move method with the coordinate and velocity variables
+	public void move (){;
+		x = x + xVelocity;
+		y = y + yVelocity;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets the sized of the ball
+	public int getSize(){
+		return size;
+	}
+	// Pre-condition: none
+	//  Post-condition: reverses the direction of the ball in the x axis when it hits the bricks 
+	public void reverseX(){
+		xVelocity -= xVelocity;
+	}
+
+	// Pre-condition: none
+	//  Post-condition: reverses the direction of the bal in the y axis when it hits bricks 
+	public void reverseY(){
+		yVelocity -= yVelocity;
+	}
 	//constructor(s):
 	
 	//methods:
--- workspace/Paddle.java
+++ workspace/Paddle.java
@@ -1,13 +1,83 @@
-import java.awt.Graphics;
 import java.awt.*;
 
 public class Paddle {
 
 	//your code here!
-	
+	private int width;
+	private int height;
+	private int x;
+	private int y;
+	private int Velocity;
+
+
+	public Paddle(int x, int y, int width, int height){
+this.x = x;
+this.y = y;
+this.width = width;
+this.height = height;
+	}
+
+
+
 	//don't forget you need instance variables:
+	// Pre-condition: height must be properly initialized
+	//  Post-condition: sets the height
+	public int getHeight() {
+		return height;
+	}
+	// Pre-condition: width must be properly initialized
+	//  Post-condition: returns the width variable value
+	public int getWidth() {
+		return width;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets the y axis on the paddle
+	public void setX(int x) {
+		this.x = x;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets the x axis on the paddle
+	public void setY(int y) {
+		this.y = y;
+	}
+	// Pre-condition: x must be properly initialized
+	//  Post-condition: returns the x variable value
+	public int getX(){
+		return x;
+	}
+	// Pre-condition: y must be properly initialized
+	//  Post-condition: returns the y variable value
+	public int getY(){
+		return y;
+	}
+	// Pre-condition: none
+	//  Post-condition: returns the velocity variable value
+	public int getVelocity(){
+		return Velocity;
+	}
 	
-	
+	// Pre-condition: velocity must be properly initialzied
+	//  Post-condition: sets the velocity
+	public void setVelocity(int velocity){
+		this.Velocity = velocity;
+	}
+
+	// Pre-condition: none
+	//  Post-condition:sets the color of the paddle
+	public void draw(Graphics g){
+	g.setColor(Color.red);
+	g.fillRect(x, y, width, height);
+	}
+	// Pre-condition: x and velocity must be properly initialized
+	//  Post-condition: sets move with x and velocity values
+	public void move (){;
+		x = x + Velocity;
+	}
+	// Pre-condition: none
+	//  Post-condition: sets addedvelocity
+	public void addVelocity(int addedVelocity){
+		this.Velocity  += addedVelocity;
+	}
 	//constructor(s):
 	
 	//methods:
